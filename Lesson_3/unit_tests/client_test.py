4c339fe3c3b27ee29447710683cfdf0f4cd8e608
Lexan <sheklford2015@yandex.ru>
2022-05-25 12:26:05 +0300

    lesson_4 done

---
+++ Lesson_3/unit_tests/client_test.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.join(os.getcwd(), '..'))
+import common.variables as variables
+from client_side import create_presence_msg, server_process_answer
+
+# Правильные данные
+ACTION_GOOD = variables.PRESENCE
+USER_GOOD = 'Lexan'
+PORT = variables.DEFAULT_PORT
+TIME = 1.1
+RESPONSE_GOOD = {variables.RESPONSE: 200}
+MESSAGE_GOOD = '200 : OK'
+
+# Неправильные данные
+USER_BAD = 'Andrey'
+RESPONSE_BAD = {variables.ERROR: 'Bad Request'}
+MESSAGE_BAD = '400 : Bad Request'
+EXPECTED_EXCEPTION = ValueError
+
+
+class TestClass(unittest.TestCase):
+
+    def test_process_answer_good(self):
+        self.assertEqual(server_process_answer(RESPONSE_GOOD), MESSAGE_GOOD)
+
+    def test_process_answer_bad(self):
+        self.assertEqual(server_process_answer(RESPONSE_BAD), MESSAGE_BAD)
+
+    def test_process_answer_error(self):
+        self.assertRaises(EXPECTED_EXCEPTION, server_process_answer, RESPONSE_BAD)
+
+    def test_create_presence_message_default(self):
+        message_main = {
+            variables.ACTION: variables.PRESENCE,
+            variables.TIME: TIME,
+            variables.PORT: variables.DEFAULT_PORT,
+            variables.USER: {
+                variables.ACCOUNT_NAME: USER_GOOD
+            }
+        }
+        message = create_presence_msg()
+        message[variables.TIME] = TIME
+        self.assertEqual(message, message_main)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
